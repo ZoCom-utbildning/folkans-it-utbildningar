c505b8c6f1b629d1e4be1f60e3591ed467bb2087
Ricky Johansson <Rickjoh91@hotmail.com>
2022-12-22 16:50:08 +0100

    Fixed radio-part of layout
    Co-Authored-By: Emil Arvidsson <92728538+KarlEmilArvid@users.noreply.github.com>

--- src/views/form/Form.tsx
+++ src/views/form/Form.tsx
@@ -1,6 +1,9 @@
 import './form.scss';
 import tempMenu from './tempMenu.svg';
-import tempPicture from './tempPicture.svg';
+import tempPicture from '../../assets/photos/tempPicture.svg';
+import hexagonEmpty from '../../assets/icons/hexagonEmpty.svg';
+import arrowLeft from '../../assets/icons/arrowLeft.svg';
+import arrowRight from '../../assets/icons/arrowRight.svg';
 
 function Form() {
 
@@ -20,11 +23,37 @@ function Form() {
 
                 <figure className="form_image">
                     <img src={ tempPicture } alt="" />
+                    <div className="gradient_overlay"></div>
                 </figure>
 
-                <article className="form_question">
-                    <p>Hur skulle du beskriva din personlighet för en person som inte känner dig, Hur skulle du beskriva din personlighet för en person som inte känner dig?</p>
-                </article>
+                <section className="quiz_section">
+
+                    <article className="form_question">
+                        <p>Hur skulle du beskriva din personlighet för en person som inte känner dig, Hur skulle du beskriva din personlighet för en person som inte känner dig?</p>
+                    </article>
+
+                    <section className="radio_quiz">
+                        <img src={ hexagonEmpty } alt="" />
+                        <p>hejje</p>
+                    </section>
+
+                    <section className="radio_quiz">
+                        <img src={ hexagonEmpty } alt="" />
+                        <p>hej</p>
+                    </section>
+
+                    <section className="radio_quiz">
+                        <img src={ hexagonEmpty } alt="" />
+                        <p>hejee</p>
+                    </section>
+
+                    <nav className="quiz_nav">
+                        <img src={ arrowLeft } alt="" />
+                        <p> 1 / 10 </p>
+                        <img src={ arrowRight } alt="" />
+                    </nav>
+
+                </section>
 
             </section>
 
