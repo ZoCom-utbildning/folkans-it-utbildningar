3da28e22e39d202ce0a60b44a72f9b634a2bdf53
Williamlof <83575469+Williamlof@users.noreply.github.com>
2022-12-22 15:23:10 +0100

    lite mer coolheter
    lite mer coolheter

--- src/components/hamburger/hamburger.tsx
+++ src/components/hamburger/hamburger.tsx
@@ -1,13 +1,42 @@
 import "./hamburger.scss";
-import Icon from "../../assets/icons/hamburger.svg";
-function Hamburger() {
+
+type Props = {
+  navOpen: boolean;
+  setNavOpen: (navOpen: boolean) => void;
+  navClass: string;
+  setNavClass: (navClass: string) => void;
+};
+
+const Hamburger = ({ setNavClass, navClass, setNavOpen, navOpen }: Props) => {
+  const toggleNav = () => {
+    // om navOpen är false när funktionen körs
+    if (!navOpen) {
+      // sätter burgaren till ett kryss
+      setNavClass("nav-icon open");
+      // nav nu öppen, sätter navOpen till true
+      setNavOpen(true);
+    } else {
+      // annars betyder det att användaren vill stänga menyn,
+      // tar bort klasser för öppen och ändrar tillbaka till hamburgaren
+      setNavOpen(false);
+      setNavClass("nav-icon");
+    }
+  };
   return (
     <div className="burger-icon">
-      <span>
-        <img src={Icon} alt="" />
-      </span>
+      <div
+        className={navClass}
+        onClick={() => {
+          toggleNav();
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   );
-}
+};
 
 export default Hamburger;
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,22 +1,45 @@
 import "./home.scss";
 import Hamburger from "../../components/hamburger/hamburger";
+import Typewriter from "typewriter-effect";
+import { useState } from "react";
 function Home() {
+  const [navOpen, setNavOpen] = useState<boolean>(false);
+  const [navClass, setNavClass] = useState<string>("nav-icon");
   return (
     <div className="wrapper">
       <header className="wrapper-header">
-        <Hamburger />
+        <Hamburger
+          navOpen={navOpen}
+          setNavOpen={setNavOpen}
+          navClass={navClass}
+          setNavClass={setNavClass}
+        />
         <h1 className="title-h1">
-          Så du är nyfiken på att jobba som utvecklare?
+          <Typewriter
+            onInit={(typeWriter) => {
+              typeWriter
+                .typeString("Så du är nyfiken på att jobba som utvecklare?")
+                .start();
+            }}
+          />
         </h1>
       </header>
       <main className="home-main">
         <div className="home-main__content">
-          <h2 className="title-h2">Vad är det som gör dig nyfiken?</h2>
+          <h4 className="title-h2">Vad är det som gör dig nyfiken?</h4>
           <p className="home-main__content__text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
             voluptas, quod, quia, voluptates quae voluptatibus quibusdam
             voluptatum quos quidem quas nesciunt. Quisquam, quae. Quisquam
             voluptas, quod, quia, voluptates quae voluptatibus quibusdam
+          </p>
+          <p>
+            Ugh ennui tacos everyday carry skateboard synth af sartorial unicorn
+            try-hard godard you probably haven't heard of them art party. Trust
+            fund live-edge banh mi, listicle seitan bicycle rights coloring book
+            try-hard austin meh tumeric. Subway tile health goth portland
+            shoreditch. Subway tile VHS messenger bag pop-up poke single-origin
+            coffee.
           </p>
         </div>
       </main>
