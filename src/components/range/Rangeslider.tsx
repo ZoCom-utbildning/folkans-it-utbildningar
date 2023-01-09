e1650736c30d5c7cf411bc2a06db9058750599cb
KarlEmilArvid <emilarvidsson96@gmail.com>
2023-01-09 14:56:00 +0100

    range slider css
    Co-Authored-By: RickyJohansson <92937315+RickyJohansson@users.noreply.github.com>

--- src/components/range/Rangeslider.tsx
+++ src/components/range/Rangeslider.tsx
@@ -2,16 +2,24 @@ import { useState } from 'react';
 import './rangeslider.scss';
 
 const RangeSlider = () => {
-    const [value, setValue] = useState(0)
+    const [value, setValue] = useState<number>(0)
+
+    const minValue = 0
+    const maxValue = 100
 
     const handleChange = (e: any) => {
         setValue(e.target.value)
     }
 
+    const handleClick = () => {
+        //
+    }
+
     return (
         <section className='range_component'>
             <p>{value}</p>
-            <input type="range" min="0" max="10" value={value} onChange={handleChange} />
+            <input className='range' type="range" min={minValue} max={maxValue} value={value} onChange={handleChange} />
+            <button className='formButton' onClick={handleClick}>Gå vidare</button>
         </section>
     )
 }
