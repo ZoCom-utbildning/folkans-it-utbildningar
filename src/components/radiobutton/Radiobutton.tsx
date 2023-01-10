516780d575236795ec43f8ac3d6d05861a2b231b
Ricky Johansson <Rickjoh91@hotmail.com>
2023-01-10 13:44:15 +0100

    Fungerande multi-step funktion
    Co-Authored-By: Emil Arvidsson <92728538+KarlEmilArvid@users.noreply.github.com>

--- src/components/radiobutton/Radiobutton.tsx
+++ src/components/radiobutton/Radiobutton.tsx
@@ -1,23 +1,27 @@
 import hexagonEmpty from '../../assets/icons/hexagonEmpty.svg';
 import './radioButton.scss';
 
-function RadioButton() {
+type Props = {
+    optionText: string;
+}
+
+function RadioButton({ optionText }: Props) {
 
     return (
         <section className='radio_component'>
             <section className="radio_quiz">
                 <img src={hexagonEmpty} alt="" />
-                <p>blå</p>
+                <p>{ optionText }</p>
             </section>
 
             <section className="radio_quiz">
                 <img src={hexagonEmpty} alt="" />
-                <p>grön</p>
+                <p>{ optionText }</p>
             </section>
 
             <section className="radio_quiz">
                 <img src={hexagonEmpty} alt="" />
-                <p>röd</p>
+                <p>{ optionText }</p>
             </section>
         </section>
     )
--- src/views/form/Form.tsx
+++ src/views/form/Form.tsx
@@ -4,9 +4,71 @@ import RadioButton from '../../components/radiobutton/Radiobutton';
 import RangeSlider from '../../components/range/Rangeslider';
 import arrowLeft from '../../assets/icons/arrowLeft.svg';
 import arrowRight from '../../assets/icons/arrowRight.svg';
+import forminfo from '../../../forminfo.json';
+import { useEffect, useState } from 'react';
 import './form.scss';
 
 function Form() {
+
+    const questions = forminfo.questions;
+    const questionNmbrs: Array<number> = [];
+
+    const [ questionId, setQuestionId] = useState<number>(1);
+    const [ formText, setFormText ] = useState<string>();
+    const [ formType, setFormType ] = useState<string>();
+    const [ optionText, setOptionText ] = useState<string>('');
+
+
+    //Changing the question depending on questionNmbr
+
+    useEffect(() => {
+
+        questions.map((question) => {
+            if (questionId === question.id) {
+                setFormText(question.text);
+            }
+        });
+
+        questions.map((question) => {
+            if (questionId === question.id) {
+                question.options.map((option) => {
+                    setOptionText(option.text);
+                })
+            }
+        });
+
+        if (questions[questionId - 1].type === 'radio') {
+            setFormType('radio');
+        }
+
+        if (questions[questionId - 1].type === 'range') {
+            setFormType('range');
+        }
+
+    }, [questionId]);
+    
+
+    // Storing array length to display maxValue of pages.
+
+    questions.map(question => {
+        questionNmbrs.push(question.id);  // [1, 2, 3, 4]
+    });
+
+
+    // Changes pagenmbrs depending on click.
+
+    const increaseQuestion = () => {
+        if (questionId < questionNmbrs.length) {
+            setQuestionId(questionId + 1);
+        }
+    }
+
+    const decreaseQuestion = () => {
+        if (questionId > 1) {
+            setQuestionId(questionId - 1);
+        }
+    }
+
 
     return (
         <div className="form_wrapper">
@@ -19,13 +81,13 @@ function Form() {
                 </section>
                 <section className="quiz_section">
                     <article className="form_question">
-                        <p>Hur skulle du beskriva din personlighet för en person som inte känner dig, Hur skulle du beskriva din personlighet för en person som inte känner dig?</p>
+                        <p>{ formText }</p>
                     </article>
-                    <RangeSlider />
+                    { formType === 'range' ? <RangeSlider optionText={optionText}/> : < RadioButton optionText={optionText}/> }
                     <nav className="quiz_nav">
-                        <img src={arrowLeft} alt="" />
-                        <p> 1 / 10 </p>
-                        <img src={arrowRight} alt="" />
+                        <img src={arrowLeft} alt="" onClick={decreaseQuestion}/>
+                        <p> { questionId } / { questionNmbrs.length } </p>
+                        <img src={arrowRight} alt="" onClick={increaseQuestion}/>
                     </nav>
                 </section>
             </section>
