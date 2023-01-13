935c27a816865deec3115bd5a5383daa96b470ee
Monica Björk <m.bjork95@gmail.com>
2023-01-13 19:16:05 +0100

    styling and tweaking

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -2,11 +2,10 @@ import './about.scss';
 import courseData from './tempInfoCourses.json';
 
 function About() {
-    console.log(courseData);
 
-    const courses = courseData.map(course => {
+    const courses = courseData.map((course, index) => {
         return(
-            <section className="course">
+            <section className="course" key={index}>
                 <h5>{course.name}</h5>
                 <p>{course.location}</p>
             </section>
@@ -15,21 +14,23 @@ function About() {
 
     return (
         <div className="about-view lines">
-            <h1>YH-utbildningar</h1>
-            <p className="introText">Gå en yrkeshögskoleutbildning inom IT på Folkuniversitetet som leder till jobb. Du kan till exempel utbilda dig till apputvecklare eller frontendutvecklare.</p>
-            <p>Alla YH-utbildningar är avgiftsfria, berättigar till studiemedel och sker i nära samverkan med arbetslivet. Utbildningarna innehåller en kvalificerad praktikperiod, LIA - lärande i arbete.</p>
+            <section className="textWrapper">
+                <h1>YH-utbildningar</h1>
+                <p className="introText">Gå en yrkeshögskoleutbildning inom IT på Folkuniversitetet som leder till jobb. Du kan till exempel utbilda dig till apputvecklare eller frontendutvecklare.</p>
+                <p>Alla YH-utbildningar är avgiftsfria, berättigar till studiemedel och sker i nära samverkan med arbetslivet. Utbildningarna innehåller en kvalificerad praktikperiod, LIA - lärande i arbete.</p>
+            </section>
 
-            <h2>Våra utbildningar</h2>
-            {courses}
+            <section className="coursesWrapper">
+                <h2>Våra utbildningar</h2>
+                {courses}
+            </section>
             
             <form className="contactForm">
-                <h2>Kontakta oss direkt</h2>
+                <h5>Kontakta oss direkt</h5>
                 <input type="email" className="formText" placeholder="din@email.com" />
-                <input type="text" className="formText" placeholder="Hej! Jag funderar på..." />
+                <textarea className="formText" placeholder="Hej! Jag funderar på..." />
                 <button className="formButton">Skicka!</button>
             </form>
-
-            <p>* footer *</p>
         </div>
     );
 }
