dbf257920ef6e07ecc7134b2e120396fe261f26a
Monica Björk <m.bjork95@gmail.com>
2023-01-13 15:00:49 +0100

    added lines class

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -14,7 +14,7 @@ function About() {
     });
 
     return (
-        <div className="about-view">
+        <div className="about-view lines">
             <h1>YH-utbildningar</h1>
             <p className="introText">Gå en yrkeshögskoleutbildning inom IT på Folkuniversitetet som leder till jobb. Du kan till exempel utbilda dig till apputvecklare eller frontendutvecklare.</p>
             <p>Alla YH-utbildningar är avgiftsfria, berättigar till studiemedel och sker i nära samverkan med arbetslivet. Utbildningarna innehåller en kvalificerad praktikperiod, LIA - lärande i arbete.</p>
