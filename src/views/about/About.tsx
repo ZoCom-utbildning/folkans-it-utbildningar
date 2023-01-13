abdbb6bf15fe630c783d42c80182d3ffb625a798
Monica Björk <m.bjork95@gmail.com>
2023-01-13 13:55:08 +0100

    Added page content and a bit of styling

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -1,10 +1,35 @@
 import './about.scss';
+import courseData from './tempInfoCourses.json';
 
 function About() {
+    console.log(courseData);
+
+    const courses = courseData.map(course => {
+        return(
+            <section className="course">
+                <h5>{course.name}</h5>
+                <p>{course.location}</p>
+            </section>
+        );
+    });
 
     return (
-        <div>
-            About
+        <div className="about-view">
+            <h1>YH-utbildningar</h1>
+            <p className="introText">Gå en yrkeshögskoleutbildning inom IT på Folkuniversitetet som leder till jobb. Du kan till exempel utbilda dig till apputvecklare eller frontendutvecklare.</p>
+            <p>Alla YH-utbildningar är avgiftsfria, berättigar till studiemedel och sker i nära samverkan med arbetslivet. Utbildningarna innehåller en kvalificerad praktikperiod, LIA - lärande i arbete.</p>
+
+            <h2>Våra utbildningar</h2>
+            {courses}
+            
+            <form className="contactForm">
+                <h2>Kontakta oss direkt</h2>
+                <input type="email" className="formText" placeholder="din@email.com" />
+                <input type="text" className="formText" placeholder="Hej! Jag funderar på..." />
+                <button className="formButton">Skicka!</button>
+            </form>
+
+            <p>* footer *</p>
         </div>
     );
 }
