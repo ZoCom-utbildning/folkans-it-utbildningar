2c0970b850ef8aa0c3c3b1cef4bc07447c48a658
Monica Björk <m.bjork95@gmail.com>
2023-01-16 19:30:05 +0100

    Added classes to make fonts smaller

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -6,7 +6,7 @@ function About() {
     const courses = courseData.map((course, index) => {
         return(
             <section className="course" key={index}>
-                <h5>{course.name}</h5>
+                <h3 className="smallerH3">{course.name}</h3>
                 <p>{course.location}</p>
             </section>
         );
@@ -22,7 +22,7 @@ function About() {
             <h1 className="line">YH-utbildningar</h1>
 
             <form className="contactForm">
-                <h5>Kontakta oss direkt</h5>
+                <h3 className="smallerText">Kontakta oss direkt</h3>
                 <input type="email" className="formText" placeholder="pelle@example.com" />
                 <textarea className="formText" rows={5} placeholder="Hej! Jag undrar ..." />
                 <button className="formButton" onClick={sendEmail}>Skicka!</button>
@@ -30,14 +30,14 @@ function About() {
 
             <p className="introText">Gå en yrkeshögskoleutbildning inom IT på Folkuniversitetet som leder till jobb. Du kan till exempel utbilda dig till apputvecklare eller frontendutvecklare.</p>
             <p>Alla YH-utbildningar är avgiftsfria, berättigar till studiemedel och sker i nära samverkan med arbetslivet. Utbildningarna innehåller en kvalificerad praktikperiod, LIA - lärande i arbete.</p>
-            <h3>Mål</h3>
+            <h2 className="smallerH2">Mål</h2>
             <p>Målen med utbildningarna är att ge dig som studerande en yrkeskompetens som leder till arbete. Du får ett examensbevis när du genomgått utbildningen. När du är färdig med utbildningen har du mycket goda möjligheter till att få omedelbar anställning. Inte sällan på det företag där du genomfört din LIA-period.</p>
 
-            <h3>Innehåll</h3>
+            <h2 className="smallerH2">Innehåll</h2>
             <p>Utbildningens innehåll består av både praktiska och teoretiska delar. Kursplaner och studieplaner tas fram tillsammans med näringslivets representanter. Under utbildningen har du utvecklingssamtal med dina lärare för att du ska få den feed-back du behöver för att utvecklas i din kommande yrkesroll.</p>
 
             <section className="coursesWrapper line">
-                <h2>Våra utbildningar</h2>
+                <h2 className="smallerH2">Våra utbildningar</h2>
                 {courses}
             </section>
         </div>
