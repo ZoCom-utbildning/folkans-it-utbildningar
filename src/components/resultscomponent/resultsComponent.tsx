b135fa2e6f6a5f956cf7a8f95c42c656dd7a8585
KarlEmilArvid <emilarvidsson96@gmail.com>
2023-01-23 15:40:05 +0100

    clean code
    Co-Authored-By: RickyJohansson <92937315+RickyJohansson@users.noreply.github.com>

--- src/components/resultscomponent/resultsComponent.tsx
+++ src/components/resultscomponent/resultsComponent.tsx
@@ -15,7 +15,7 @@ const ResultsComponent = () => {
         <div className="results_wrapper">
             <section className="results_section">
                 <h3>Ditt test resultat blev: </h3>
-                <p> { courseResult } </p>
+                <p> {courseResult} </p>
                 <p>länkar till utbildningen</p>
                 < TestButton buttonText={'ta testet igen'} /> {/** rensa localstorage här? */}
             </section>
@@ -23,7 +23,7 @@ const ResultsComponent = () => {
                 <h3>Alternativa utbildningar </h3>
                 <p>Länkar till utbildningarna: </p>
                 <li>
-                    <span>{ points }P: <a href="" className="form_link">Frontend-utvecklare</a></span>
+                    <span>{points}P: <a href="" className="form_link">Frontend-utvecklare</a></span>
                     <span>12P: <a href="" className="form_link">Backend-utvecklare</a></span>
                     <span>10P: <a href="" className="form_link">Fullstack-utvecklare</a></span>
                     <span>7P: <a href="" className="form_link">Mjukvaru-utvecklare</a></span>
