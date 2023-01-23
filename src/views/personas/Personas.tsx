cd20536c79f988bfae4b67ad70d298205d66b20d
Williamlof <83575469+Williamlof@users.noreply.github.com>
2023-01-23 13:13:25 +0100

    Semi-done layout personas
    Semi-done layout personas

--- src/views/personas/Personas.tsx
+++ src/views/personas/Personas.tsx
@@ -1,7 +1,84 @@
-import "./Personas.scss";
+import "./personas.scss";
 
 function Personas() {
-  return <div className="personasView-container"></div>;
+  return (
+    <div className="personasView-container">
+      <section className="personasView">
+        <h1 className="title-h1">Möt några av våra studerande</h1>
+        <main className="personas-main">
+          <article className="personas-card">
+            <section className="personas-card-header">
+              <img src="https://via.placeholder.com/150" alt="" />
+              <h2 className="personas-card-title">Adam, 20</h2>
+            </section>
+            <main className="personas-card-text-container">
+              <p className="personas-card-ingress">
+                Adam är en av de personer som startat sin utbildning hos
+                Folkuniversitetet.
+              </p>
+              <p className="personas-card-text">
+                Här är en text som beskriver Adam och hur han kom in på
+                utbildningensom förändrade hans liv till det bättre. Adam är en
+                riktig kung som har lärt sig programmera på egen hand men ville
+                utvecklas ytterliggare så han sökte in på frontend-utbildningen
+                via folkuniversitetets hemsida, efter att han tagit quizet och
+                fått reda på att han skulle passa perfekt inom
+                frontend-utbildningen.
+              </p>
+            </main>
+          </article>
+          <article className="personas-card">
+            <section className="personas-card-header">
+              <img src="https://via.placeholder.com/150" alt="" />
+              <h2 className="personas-card-title">Adam, 20</h2>
+            </section>
+            <main className="personas-card-text-container">
+              <p className="personas-card-ingress">
+                Adam är en av de personer som startat sin utbildning hos
+                Folkuniversitetet.
+              </p>
+              <p className="personas-card-text">
+                Här är en text som beskriver Adam och hur han kom in på
+                utbildningensom förändrade hans liv till det bättre.
+              </p>
+            </main>
+          </article>
+          <article className="personas-card">
+            <section className="personas-card-header">
+              <img src="https://via.placeholder.com/150" alt="" />
+              <h2 className="personas-card-title">Adam, 20</h2>
+            </section>
+            <main className="personas-card-text-container">
+              <p className="personas-card-ingress">
+                Adam är en av de personer som startat sin utbildning hos
+                Folkuniversitetet.
+              </p>
+              <p className="personas-card-text">
+                Här är en text som beskriver Adam och hur han kom in på
+                utbildningensom förändrade hans liv till det bättre.
+              </p>
+            </main>
+          </article>
+          <article className="personas-card">
+            <section className="personas-card-header">
+              <img src="https://via.placeholder.com/150" alt="" />
+              <h2 className="personas-card-title">Adam, 20</h2>
+            </section>
+            <main className="personas-card-text-container">
+              <p className="personas-card-ingress">
+                Adam är en av de personer som startat sin utbildning hos
+                Folkuniversitetet.
+              </p>
+              <p className="personas-card-text">
+                Här är en text som beskriver Adam och hur han kom in på
+                utbildningensom förändrade hans liv till det bättre.
+              </p>
+            </main>
+          </article>
+        </main>
+      </section>
+    </div>
+  );
 }
 
 export default Personas;
