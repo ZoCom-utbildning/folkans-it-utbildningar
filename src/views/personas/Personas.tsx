60eaf094e141d6cca5ef37029dffa82ecf1cd072
Williamlof <83575469+Williamlof@users.noreply.github.com>
2023-01-25 21:22:23 +0100

    Lagt in mixins, persona bilder samt gjort del av animationen.
    Lagt in mixins, persona bilder samt gjort del av animationen.

--- src/views/personas/Personas.tsx
+++ src/views/personas/Personas.tsx
@@ -1,93 +1,108 @@
 import "./personas.scss";
-
+import { useRef, useEffect } from "react";
 function Personas() {
+  const contRef = useRef<HTMLDivElement>(null);
+  const elsArrRef = useRef<HTMLDivElement[]>([]);
+  const closeBtnsArrRef = useRef<HTMLButtonElement[]>([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (contRef.current) contRef.current.classList.remove("s--inactive");
+    }, 200);
+
+    elsArrRef.current = elsArrRef.current.filter(Boolean);
+    elsArrRef.current.forEach((el) => {
+      el.addEventListener("click", handleElClick);
+    });
+
+    closeBtnsArrRef.current = closeBtnsArrRef.current.filter(Boolean);
+    closeBtnsArrRef.current.forEach((btn) => {
+      btn.addEventListener("click", handleCloseBtnClick);
+    });
+
+    return () => {
+      elsArrRef.current.forEach((el) => {
+        el.removeEventListener("click", handleElClick);
+      });
+
+      closeBtnsArrRef.current.forEach((btn) => {
+        btn.removeEventListener("click", handleCloseBtnClick);
+      });
+    };
+  }, []);
+
+  const handleElClick = (e: MouseEvent) => {
+    const target = e.currentTarget as HTMLDivElement;
+    if (!target.classList.contains("s--active")) {
+      elsArrRef.current.forEach((el) => {
+        if (el.classList.contains("s--active")) {
+          el.classList.remove("s--active");
+        }
+      });
+      target.classList.add("s--active");
+    }
+  };
+
+  const handleCloseBtnClick = (e: MouseEvent) => {
+    e.stopPropagation();
+    elsArrRef.current.forEach((el) => {
+      if (el.classList.contains("s--active")) {
+        el.classList.remove("s--active");
+      }
+    });
+  };
+
   return (
     <div className="personasView-container">
       <section className="personasView">
         <h1 className="title-h1">Möt några av våra studerande</h1>
-        {/* <main className="personas-main">
-          <article className="personas-card">
-            <section className="personas-card-header">
-              <img src="https://via.placeholder.com/150" alt="" />
-              <h2 className="personas-card-title">Adam, 20</h2>
-            </section>
-            <main className="personas-card-text-container">
-              <p className="personas-card-ingress">
-                Adam är en av de personer som startat sin utbildning hos
-                Folkuniversitetet.
-              </p>
-              <p className="personas-card-text">
-                Här är en text som beskriver Adam och hur han kom in på
-                utbildningensom förändrade hans liv till det bättre. Adam är en
-                riktig kung som har lärt sig programmera på egen hand men ville
-                utvecklas ytterliggare så han sökte in på frontend-utbildningen
-                via folkuniversitetets hemsida, efter att han tagit quizet och
-                fått reda på att han skulle passa perfekt inom
-                frontend-utbildningen.
-              </p>
-            </main>
-          </article>
-          <article className="personas-card">
-            <section className="personas-card-header">
-              <img src="https://via.placeholder.com/150" alt="" />
-              <h2 className="personas-card-title">Adam, 20</h2>
-            </section>
-            <main className="personas-card-text-container">
-              <p className="personas-card-ingress">
-                Adam är en av de personer som startat sin utbildning hos
-                Folkuniversitetet.
-              </p>
-              <p className="personas-card-text">
-                Här är en text som beskriver Adam och hur han kom in på
-                utbildningensom förändrade hans liv till det bättre.
-              </p>
-            </main>
-          </article>
-          <article className="personas-card">
-            <section className="personas-card-header">
-              <img src="https://via.placeholder.com/150" alt="" />
-              <h2 className="personas-card-title">Adam, 20</h2>
-            </section>
-            <main className="personas-card-text-container">
-              <p className="personas-card-ingress">
-                Adam är en av de personer som startat sin utbildning hos
-                Folkuniversitetet.
-              </p>
-              <p className="personas-card-text">
-                Här är en text som beskriver Adam och hur han kom in på
-                utbildningensom förändrade hans liv till det bättre.
-              </p>
-            </main>
-          </article>
-          <article className="personas-card">
-            <section className="personas-card-header">
-              <img src="https://via.placeholder.com/150" alt="" />
-              <h2 className="personas-card-title">Adam, 20</h2>
-            </section>
-            <main className="personas-card-text-container">
-              <p className="personas-card-ingress">
-                Adam är en av de personer som startat sin utbildning hos
-                Folkuniversitetet.
-              </p>
-              <p className="personas-card-text">
-                Här är en text som beskriver Adam och hur han kom in på
-                utbildningensom förändrade hans liv till det bättre.
-              </p>
-            </main>
-          </article>
-        </main> */}
-        <div className="cont s--inactive">
+
+        <div className="cont" ref={contRef}>
           <div className="cont__inner">
-            <div className="el">
+            <div
+              className="el"
+              ref={(el) => elsArrRef.current.push(el as HTMLDivElement)}
+            >
               <div className="el__overflow">
                 <div className="el__inner">
                   <div className="el__bg"></div>
-                  <div className="el__preview-cont">
-                    <h2 className="el__heading">Section 1</h2>
-                  </div>
+                  <section className="el__preview-cont">
+                    <h2 className="el__heading">Adam, 20</h2>
+                  </section>
                   <div className="el__content">
-                    <div className="el__text">Whatever</div>
-                    <div className="el__close-btn"></div>
+                    <div className="personas-content-container">
+                      <article className="personas-card">
+                        <section className="personas-card-header">
+                          <img src="https://via.placeholder.com/150" alt="" />
+                          <h2 className="personas-card-title">Adam, 20</h2>
+                        </section>
+                        <main className="personas-card-text-container">
+                          <p className="personas-card-ingress">
+                            Adam är en av de personer som startat sin utbildning
+                            hos Folkuniversitetet.
+                          </p>
+                          <p className="personas-card-text">
+                            Här är en text som beskriver Adam och hur han kom in
+                            på utbildningensom förändrade hans liv till det
+                            bättre. Adam är en riktig kung som har lärt sig
+                            programmera på egen hand men ville utvecklas
+                            ytterliggare så han sökte in på
+                            frontend-utbildningen via folkuniversitetets
+                            hemsida, efter att han tagit quizet och fått reda på
+                            att han skulle passa perfekt inom
+                            frontend-utbildningen.
+                          </p>
+                        </main>
+                      </article>
+                      <div
+                        className="el__close-btn"
+                        ref={(btn) =>
+                          closeBtnsArrRef.current.push(
+                            btn as unknown as HTMLButtonElement
+                          )
+                        }
+                      ></div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -101,16 +116,26 @@ function Personas() {
               </div>
             </div>
 
-            <div className="el">
+            <div
+              className="el"
+              ref={(el) => elsArrRef.current.push(el as HTMLDivElement)}
+            >
               <div className="el__overflow">
                 <div className="el__inner">
                   <div className="el__bg"></div>
-                  <div className="el__preview-cont">
-                    <h2 className="el__heading">Section 2</h2>
-                  </div>
+                  <section className="el__preview-cont">
+                    <h2 className="el__heading">Bertil, 30</h2>
+                  </section>
                   <div className="el__content">
                     <div className="el__text">Whatever</div>
-                    <div className="el__close-btn"></div>
+                    <div
+                      className="el__close-btn"
+                      ref={(btn) =>
+                        closeBtnsArrRef.current.push(
+                          btn as unknown as HTMLButtonElement
+                        )
+                      }
+                    ></div>
                   </div>
                 </div>
               </div>
@@ -124,16 +149,26 @@ function Personas() {
               </div>
             </div>
 
-            <div className="el">
+            <div
+              className="el"
+              ref={(el) => elsArrRef.current.push(el as HTMLDivElement)}
+            >
               <div className="el__overflow">
                 <div className="el__inner">
                   <div className="el__bg"></div>
-                  <div className="el__preview-cont">
-                    <h2 className="el__heading">Section 3</h2>
-                  </div>
+                  <section className="el__preview-cont">
+                    <h2 className="el__heading">Ahmed, 32</h2>
+                  </section>
                   <div className="el__content">
                     <div className="el__text">Whatever</div>
-                    <div className="el__close-btn"></div>
+                    <div
+                      className="el__close-btn"
+                      ref={(btn) =>
+                        closeBtnsArrRef.current.push(
+                          btn as unknown as HTMLButtonElement
+                        )
+                      }
+                    ></div>
                   </div>
                 </div>
               </div>
@@ -147,16 +182,26 @@ function Personas() {
               </div>
             </div>
 
-            <div className="el">
+            <div
+              className="el"
+              ref={(el) => elsArrRef.current.push(el as HTMLDivElement)}
+            >
               <div className="el__overflow">
                 <div className="el__inner">
                   <div className="el__bg"></div>
-                  <div className="el__preview-cont">
-                    <h2 className="el__heading">Section 4</h2>
-                  </div>
+                  <section className="el__preview-cont">
+                    <h2 className="el__heading">Anja, 23</h2>
+                  </section>
                   <div className="el__content">
                     <div className="el__text">Whatever</div>
-                    <div className="el__close-btn"></div>
+                    <div
+                      className="el__close-btn"
+                      ref={(btn) =>
+                        closeBtnsArrRef.current.push(
+                          btn as unknown as HTMLButtonElement
+                        )
+                      }
+                    ></div>
                   </div>
                 </div>
               </div>
@@ -170,16 +215,26 @@ function Personas() {
               </div>
             </div>
 
-            <div className="el">
+            <div
+              className="el"
+              ref={(el) => elsArrRef.current.push(el as HTMLDivElement)}
+            >
               <div className="el__overflow">
                 <div className="el__inner">
                   <div className="el__bg"></div>
-                  <div className="el__preview-cont">
-                    <h2 className="el__heading">Section 5</h2>
-                  </div>
+                  <section className="el__preview-cont">
+                    <h2 className="el__heading">Amina, 38</h2>
+                  </section>
                   <div className="el__content">
                     <div className="el__text">Whatever</div>
-                    <div className="el__close-btn"></div>
+                    <div
+                      className="el__close-btn"
+                      ref={(btn) =>
+                        closeBtnsArrRef.current.push(
+                          btn as unknown as HTMLButtonElement
+                        )
+                      }
+                    ></div>
                   </div>
                 </div>
               </div>
