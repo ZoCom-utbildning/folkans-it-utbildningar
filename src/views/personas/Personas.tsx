c4a4ccebce14338eb0839686d6568b31bad672af
Williamlof <83575469+Williamlof@users.noreply.github.com>
2023-01-25 10:44:47 +0100

    Variables for personas

--- src/views/personas/Personas.tsx
+++ src/views/personas/Personas.tsx
@@ -5,7 +5,7 @@ function Personas() {
     <div className="personasView-container">
       <section className="personasView">
         <h1 className="title-h1">Möt några av våra studerande</h1>
-        <main className="personas-main">
+        {/* <main className="personas-main">
           <article className="personas-card">
             <section className="personas-card-header">
               <img src="https://via.placeholder.com/150" alt="" />
@@ -75,7 +75,125 @@ function Personas() {
               </p>
             </main>
           </article>
-        </main>
+        </main> */}
+        <div className="cont s--inactive">
+          <div className="cont__inner">
+            <div className="el">
+              <div className="el__overflow">
+                <div className="el__inner">
+                  <div className="el__bg"></div>
+                  <div className="el__preview-cont">
+                    <h2 className="el__heading">Section 1</h2>
+                  </div>
+                  <div className="el__content">
+                    <div className="el__text">Whatever</div>
+                    <div className="el__close-btn"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="el__index">
+                <div className="el__index-back">1</div>
+                <div className="el__index-front">
+                  <div className="el__index-overlay" data-index="1">
+                    1
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="el">
+              <div className="el__overflow">
+                <div className="el__inner">
+                  <div className="el__bg"></div>
+                  <div className="el__preview-cont">
+                    <h2 className="el__heading">Section 2</h2>
+                  </div>
+                  <div className="el__content">
+                    <div className="el__text">Whatever</div>
+                    <div className="el__close-btn"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="el__index">
+                <div className="el__index-back">2</div>
+                <div className="el__index-front">
+                  <div className="el__index-overlay" data-index="2">
+                    2
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="el">
+              <div className="el__overflow">
+                <div className="el__inner">
+                  <div className="el__bg"></div>
+                  <div className="el__preview-cont">
+                    <h2 className="el__heading">Section 3</h2>
+                  </div>
+                  <div className="el__content">
+                    <div className="el__text">Whatever</div>
+                    <div className="el__close-btn"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="el__index">
+                <div className="el__index-back">3</div>
+                <div className="el__index-front">
+                  <div className="el__index-overlay" data-index="3">
+                    3
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="el">
+              <div className="el__overflow">
+                <div className="el__inner">
+                  <div className="el__bg"></div>
+                  <div className="el__preview-cont">
+                    <h2 className="el__heading">Section 4</h2>
+                  </div>
+                  <div className="el__content">
+                    <div className="el__text">Whatever</div>
+                    <div className="el__close-btn"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="el__index">
+                <div className="el__index-back">4</div>
+                <div className="el__index-front">
+                  <div className="el__index-overlay" data-index="4">
+                    4
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="el">
+              <div className="el__overflow">
+                <div className="el__inner">
+                  <div className="el__bg"></div>
+                  <div className="el__preview-cont">
+                    <h2 className="el__heading">Section 5</h2>
+                  </div>
+                  <div className="el__content">
+                    <div className="el__text">Whatever</div>
+                    <div className="el__close-btn"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="el__index">
+                <div className="el__index-back">5</div>
+                <div className="el__index-front">
+                  <div className="el__index-overlay" data-index="5">
+                    5
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
