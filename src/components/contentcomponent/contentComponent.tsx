acaea9eb9776ca5670f19ce9c9c3ff369dbe6453
Williamlof <83575469+Williamlof@users.noreply.github.com>
2023-02-01 15:00:19 +0100

    Testet fungerar
    Fixat krasch, frågorna laddas in via firebase
    
    Co-Authored-By: RickyJohansson <92937315+RickyJohansson@users.noreply.github.com>
    Co-Authored-By: Emil Arvidsson <92728538+KarlEmilArvid@users.noreply.github.com>

--- src/components/contentcomponent/contentComponent.tsx
+++ src/components/contentcomponent/contentComponent.tsx
@@ -1,181 +1,229 @@
-import arrowLeft from '../../assets/icons/arrowLeft.svg';
-import arrowRight from '../../assets/icons/arrowRight.svg';
-import forminfo from '../../../forminfo.json';
-import { SetStateAction, useEffect, useState } from 'react';
-import "./contentcomponent.scss"
-import { auth, db } from '../../services/firebase';
-import { addDoc, collection } from 'firebase/firestore';
+import arrowLeft from "../../assets/icons/arrowLeft.svg";
+import arrowRight from "../../assets/icons/arrowRight.svg";
+import forminfo from "../../../forminfo.json";
+import { SetStateAction, useEffect, useState } from "react";
+import "./contentcomponent.scss";
+import { auth, db } from "../../services/firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 type Props = {
-    formText: string;
-    questionId: number;
-    firstQuestion: boolean;
-    decreaseQuestion: () => void
-    increaseQuestion: (toggle: boolean) => void
-}
+  questions: Array<any>;
+  formText: string;
+  questionId: number;
+  firstQuestion: boolean;
+  decreaseQuestion: () => void;
+  increaseQuestion: (toggle: boolean) => void;
+};
 
 type ResultsType = {
-    question: string;
-    button: string;
-    points: string[];
-}
+  question: string;
+  button: string;
+  points: string[];
+};
 
-function ContentComponent({ formText, questionId, decreaseQuestion, increaseQuestion, firstQuestion }: Props) {
+function ContentComponent({
+  questions,
+  formText,
+  questionId,
+  decreaseQuestion,
+  increaseQuestion,
+  firstQuestion,
+}: Props) {
+  const [resultsArray, setResultsArray] = useState<ResultsType[]>(
+    JSON.parse(localStorage.getItem("resultsArray")!) || []
+  );
+  const [optionText, setOptionText] = useState<string[]>([]);
+  const [buttonId, setButtonId] = useState<string>("0");
+  const [pointsId, setPointsId] = useState<string[]>([]);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [buttonCheck, setButtonCheck] = useState<boolean>(false);
+  const [toggle, setToggle] = useState<boolean>(false);
 
-    const questions = forminfo.questions;
-    const questionNmbrs: Array<number> = [];
+  const resultsValue: ResultsType = {
+    question: `${questionId}`,
+    button: `${buttonId}`,
+    points: pointsId,
+  };
 
-    const [resultsArray, setResultsArray] = useState<ResultsType[]>(JSON.parse(localStorage.getItem("resultsArray")!) || []);
-    const [optionText, setOptionText] = useState<string[]>([])
-    const [buttonId, setButtonId] = useState<string>("0")
-    const [pointsId, setPointsId] = useState<string[]>([])
-    const [selectedOption, setSelectedOption] = useState("")
-    const [buttonCheck, setButtonCheck] = useState<boolean>(false)
-    const [toggle, setToggle] = useState<boolean>(false)
-
-    const resultsValue: ResultsType = {
-        question: `${questionId}`,
-        button: `${buttonId}`,
-        points: pointsId
+  useEffect(() => {
+    if (!resultsArray[questionId - 1] && questionId !== 0) {
+      const updatedArray: ResultsType[] = [
+        ...resultsArray,
+        { question: "", button: "", points: [] },
+      ];
+      setResultsArray(updatedArray);
+      localStorage.setItem("resultsArray", JSON.stringify(updatedArray));
     }
+  }, []);
 
-    useEffect(() => {
-        if (!resultsArray[questionId - 1] && questionId !== 0) {
-            const updatedArray: ResultsType[] = [...resultsArray, { question: "", button: "", points: [] }];
-            setResultsArray(updatedArray);
-            localStorage.setItem("resultsArray", JSON.stringify(updatedArray));
-        }
-    }, [])
+  useEffect(() => {
+    questions.map((question) => {
+      if (questionId === question.id) {
+        const tempText: SetStateAction<string[]> = [];
+        question.options.map((option: { text: string }) => {
+          tempText.push(option.text);
+        });
+        setOptionText(tempText);
+      }
+    });
+    setButtonCheck(false);
+  }, [questionId]);
 
-    useEffect(() => {
-        questions.map(question => {
-            if (questionId === question.id) {
-                const tempText: SetStateAction<string[]> = []
-                question.options.map(option => {
-                    tempText.push(option.text)
-                })
-                setOptionText(tempText)
-            }
-        })
-        setButtonCheck(false);
-    }, [questionId])
-
-    useEffect(() => {
-        if (resultsArray.length > 0 && resultsArray[questionId]) {
-            setSelectedOption(resultsArray[questionId].button)
-            setButtonCheck(true);
-
-        } else {
-            setSelectedOption("")
-        }
-    }, [questionId])
-
-    useEffect(() => {
-        if (toggle) {
-            const index = resultsArray.findIndex(obj => obj.question === resultsValue.question);
-            if (index === -1) {
-                // lägg till nytt objekt
-                const updatedArray: ResultsType[] = [...resultsArray, resultsValue];
-                setResultsArray(updatedArray);
-                localStorage.setItem("resultsArray", JSON.stringify(updatedArray));
-            } else {
-                // uppdatera arrayen med nytt objekt-värde
-                const updatedArray = [
-                    ...resultsArray.slice(0, index),
-                    resultsValue,
-                    ...resultsArray.slice(index + 1)
-                ];
-                setResultsArray(updatedArray);
-                localStorage.setItem("resultsArray", JSON.stringify(updatedArray));
-            }
-            setToggle(!toggle)
-        }
-    }, [pointsId])
-
-    const temporaryPoints = async (buttonId: any) => {
-        const id = await buttonId
-
-        questions.map(question => {
-            if (questionId === question.id) {
-                const tempPoints: SetStateAction<string[]> = []
-                question.options.map(option => {
-                    if (option.id == Number(id) + 1) {
-                        option.value.map((value) => {
-                            tempPoints.push(`${value.points}`);
-                        })
-                    }
-                })
-                if (resultsArray.length > 0) {
-                    setPointsId(tempPoints)
-                }
-            }
-        })
+  useEffect(() => {
+    if (resultsArray.length > 0 && resultsArray[questionId]) {
+      setSelectedOption(resultsArray[questionId].button);
+      setButtonCheck(true);
+    } else {
+      setSelectedOption("");
     }
+  }, [questionId]);
 
-    async function writeToDB() {
-        const user = auth.currentUser?.uid;
-        console.log("user uid", user);
+  useEffect(() => {
+    if (toggle) {
+      const index = resultsArray.findIndex(
+        (obj) => obj.question === resultsValue.question
+      );
+      if (index === -1) {
+        // lägg till nytt objekt
+        const updatedArray: ResultsType[] = [...resultsArray, resultsValue];
+        setResultsArray(updatedArray);
+        localStorage.setItem("resultsArray", JSON.stringify(updatedArray));
+      } else {
+        // uppdatera arrayen med nytt objekt-värde
+        const updatedArray = [
+          ...resultsArray.slice(0, index),
+          resultsValue,
+          ...resultsArray.slice(index + 1),
+        ];
+        setResultsArray(updatedArray);
+        localStorage.setItem("resultsArray", JSON.stringify(updatedArray));
+      }
+      setToggle(!toggle);
+    }
+  }, [pointsId]);
 
-        try {
-            const docRef = await addDoc(collection(db, "answers"), {
-              first: "Alan",
-              middle: "Mathison",
-              last: "Turing",
-              born: 1912
+  const temporaryPoints = async (buttonId: any) => {
+    const id = await buttonId;
+
+    questions.map((question) => {
+      if (questionId === question.id) {
+        const tempPoints: SetStateAction<string[]> = [];
+        question.options.map((option: { id: number; value: any[] }) => {
+          if (option.id == Number(id) + 1) {
+            option.value.map((value) => {
+              tempPoints.push(`${value.points}`);
             });
-          
-            console.log("Document written with ID: ", docRef.id);
-          } catch (e) {
-            console.error("Error adding document: ", e);
           }
-          
+        });
+        if (resultsArray.length > 0) {
+          setPointsId(tempPoints);
+        }
+      }
+    });
+  };
+
+  async function writeToDB() {
+    const user = auth.currentUser?.uid;
+    console.log("user uid", user);
+
+    try {
+      const docRef = await addDoc(collection(db, "answers"), {
+        first: "Alan",
+        middle: "Mathison",
+        last: "Turing",
+        born: 1912,
+      });
+
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
+  }
 
-    const radioClicked = (number: any) => {
-        setButtonId(number)
-        temporaryPoints(number)
-        setToggle(!toggle)
-        setButtonCheck(true)
-        writeToDB();
-    }
+  const radioClicked = (number: any) => {
+    setButtonId(number);
+    temporaryPoints(number);
+    setToggle(!toggle);
+    setButtonCheck(true);
+    writeToDB();
+  };
 
-    const handleOption = (event: any) => {
-        setSelectedOption(event.target.value);
-        console.log(event.target.checked)
-    }
+  const handleOption = (event: any) => {
+    setSelectedOption(event.target.value);
+  };
 
-
-    // Storing array length to display maxValue of pages.
-    questions.map(question => {
-        questionNmbrs.push(question.id);  // [1, 2, 3, 4]
-    })
-
-    return (
-        <section className="quiz_section">
-            <article className="form_question">
-                <p>{formText}</p>
-            </article>
-            <section className='radio_wrapper'>
-                <label htmlFor="radio-0" className='radio_component' onClick={() => radioClicked(0)}>
-                    <input type='radio' name='radio' id="radio-0" value="0" checked={selectedOption === "0"} onChange={handleOption}></input>
-                    <p>{optionText[0]}</p>
-                </label>
-                <label htmlFor="radio-1" className='radio_component' onClick={() => radioClicked(1)}>
-                    <input type='radio' name='radio' id="radio-1" value="1" checked={selectedOption === "1"} onChange={handleOption}></input>
-                    <p>{optionText[1]}</p>
-                </label>
-                <label htmlFor="radio-2" className='radio_component' onClick={() => radioClicked(2)}>
-                    <input type='radio' name='radio' id="radio-2" value="2" checked={selectedOption === "2"} onChange={handleOption}></input>
-                    <p>{optionText[2]}</p>
-                </label>
-            </section>
-            <nav className="quiz_nav">
-                <img className={`hidden_${firstQuestion ? "true" : "false"}`} src={arrowLeft} alt="" onClick={() => decreaseQuestion()} />
-                <p> {questionId} / {questionNmbrs.length - 1} </p>
-                <img className={`transparent_${buttonCheck ? "false" : "true"}`} src={arrowRight} alt="" onClick={() => increaseQuestion(buttonCheck)} />
-            </nav>
-        </section >
-    )
+  return (
+    <section className="quiz_section">
+      <article className="form_question">
+        <p>{formText}</p>
+      </article>
+      <section className="radio_wrapper">
+        <label
+          htmlFor="radio-0"
+          className="radio_component"
+          onClick={() => radioClicked(0)}
+        >
+          <input
+            type="radio"
+            name="radio"
+            id="radio-0"
+            value="0"
+            checked={selectedOption === "0"}
+            onChange={handleOption}
+          ></input>
+          <p>{optionText[0]}</p>
+        </label>
+        <label
+          htmlFor="radio-1"
+          className="radio_component"
+          onClick={() => radioClicked(1)}
+        >
+          <input
+            type="radio"
+            name="radio"
+            id="radio-1"
+            value="1"
+            checked={selectedOption === "1"}
+            onChange={handleOption}
+          ></input>
+          <p>{optionText[1]}</p>
+        </label>
+        <label
+          htmlFor="radio-2"
+          className="radio_component"
+          onClick={() => radioClicked(2)}
+        >
+          <input
+            type="radio"
+            name="radio"
+            id="radio-2"
+            value="2"
+            checked={selectedOption === "2"}
+            onChange={handleOption}
+          ></input>
+          <p>{optionText[2]}</p>
+        </label>
+      </section>
+      <nav className="quiz_nav">
+        <img
+          className={`hidden_${firstQuestion ? "true" : "false"}`}
+          src={arrowLeft}
+          alt=""
+          onClick={() => decreaseQuestion()}
+        />
+        <p>
+          {" "}
+          {questionId} / {questions.length - 1}{" "}
+        </p>
+        <img
+          className={`transparent_${buttonCheck ? "false" : "true"}`}
+          src={arrowRight}
+          alt=""
+          onClick={() => increaseQuestion(buttonCheck)}
+        />
+      </nav>
+    </section>
+  );
 }
 
 export default ContentComponent;
--- src/components/formcomponent/formComponent.tsx
+++ src/components/formcomponent/formComponent.tsx
@@ -10,123 +10,114 @@ import { db } from "../../services/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 type Props = {
-	activePersona: number;
+  activePersona: number;
 };
 
 function FormComponent({ activePersona }: Props) {
-	// const questions = forminfo.questions;
-	const questionNmbrs: Array<number> = [];
+  // const questions = forminfo.questions;
+  // const questionNmbrs: Array<number> = [];
+  const AMOUNT_OF_QUESTIONS = 11;
+  const [questions, setQuestions] = useState<Array<any>>([]);
+  const [questionId, setQuestionId] = useState<number>(0);
+  const [formText, setFormText] = useState<string>("");
+  const [formImage, setFormImage] = useState<string>("");
+  const [altImage, setAltImage] = useState<string>("");
+  const [lastPage, setLastPage] = useState<boolean>(false);
+  const [firstPage, setFirstPage] = useState<boolean>(false);
+  const [firstQuestion, setFirstQuestion] = useState<boolean>(true);
 
-	const [questions, setQuestions] = useState<Array<any>>([]);
-	const [questionId, setQuestionId] = useState<number>(0);
-	const [formText, setFormText] = useState<string>("");
-	const [formImage, setFormImage] = useState<string>("");
-	const [altImage, setAltImage] = useState<string>("");
-	const [lastPage, setLastPage] = useState<boolean>(false);
-	const [firstPage, setFirstPage] = useState<boolean>(false);
-	const [firstQuestion, setFirstQuestion] = useState<boolean>(true)
+  useEffect(() => {
+    (async () => {
+      const querySnapshot = await getDocs(collection(db, "questions"));
+      const tempArr: any[] = [];
+      querySnapshot.forEach((doc) => {
+        tempArr.push(doc.data());
+      });
 
+      setQuestions(tempArr);
+    })();
+  }, []);
 
-	useEffect(() => {
-		(async () => {
-			const querySnapshot = await getDocs(collection(db, "questions"));
-			const tempArr:any[] = [];
-			querySnapshot.forEach((doc) => {
-				tempArr.push(doc.data());
-			});
-			console.log("tempArr.length", tempArr.length);
-			setQuestions(tempArr);
-		})();
-		console.log("questions.length", questions.length);
-	}, []);
+  //Changes question depending on questionNmbr
+  useEffect(() => {
+    questions.map((question) => {
+      if (questionId === question.id) {
+        setFormText(question.text);
+        setFormImage(question.img);
+        setAltImage(question.alt);
+      }
+    });
 
+    if (!window.location.href.includes("fragor")) {
+      setFormImage(happyGuy);
+    }
 
-	//Changes question depending on questionNmbr
-	useEffect(() => {
-		console.log("questionNmbrs", questionNmbrs);
-		
-		// Storing array length to display maxValue of pages.
-		questions.map((question) => {
-			questionNmbrs.push(question.id); // [1, 2, 3, 4]
-		});
+    if (questionId === AMOUNT_OF_QUESTIONS) {
+      setLastPage(true);
+    }
 
-		questions.map((question) => {
-			if (questionId === question.id) {
-				setFormText(question.text);
-				setFormImage(question.img);
-				setAltImage(question.alt);
-			}
-		});
+    if (questionId === 0) {
+      setFirstPage(true);
+    } else {
+      setFirstPage(false);
+    }
 
-		if (!window.location.href.includes("fragor")) {
-			setFormImage(happyGuy);
-		}
+    if (questionId === 1) {
+      setFirstQuestion(true);
+    } else {
+      setFirstQuestion(false);
+    }
+  }, [questionId, questions]);
 
-		if (questionId === questionNmbrs[questionNmbrs.length - 1]) {
-			setLastPage(true);
-		}
+  // Changes pagenmbrs depending on click.
+  //framåt knapp syns inte om du inte svarat på frågan
+  const increaseQuestion = (buttonCheck: boolean) => {
+    if (questionId < questions.length && buttonCheck) {
+      setQuestionId(questionId + 1);
+      setFirstQuestion(false);
+    }
+  };
 
-		if (questionId === 0) {
-			setFirstPage(true);
-		} else {
-			setFirstPage(false)
-		}
+  const decreaseQuestion = () => {
+    if (questionId > 1) {
+      setQuestionId(questionId - 1);
+    }
+  };
 
-		if (questionId === 1) {
-			setFirstQuestion(true)
-		} else {
-			setFirstQuestion(false)
-		}
-	}, [questionId, questions]);
+  const startTest = () => {
+    setQuestionId(questionId + 1);
+  };
 
-	// Changes pagenmbrs depending on click.
-	//framåt knapp syns inte om du inte svarat på frågan
-	const increaseQuestion = (buttonCheck: boolean) => {
-		if (questionId < questionNmbrs.length && buttonCheck) {
-			setQuestionId(questionId + 1);
-			setFirstQuestion(false)
-		}
-	}
-
-	const decreaseQuestion = () => {
-		if (questionId > 1) {
-			setQuestionId(questionId - 1);
-		}
-	}
-
-	const startTest = () => {
-		setQuestionId(questionId + 1);
-	}
-
-	return (
-		<section className="card_content">
-			{window.location.href.includes("fragor") && firstPage ? (
-				<>
-					<OnboardingComponent startTest={startTest} />
-				</>
-			) : window.location.href.includes("fragor") && !firstPage && !lastPage ? (
-				<>
-					<ImageComponent formImage={formImage} altImage={altImage} />
-					<ContentComponent
-						formText={formText}
-						questionId={questionId}
-						increaseQuestion={increaseQuestion}
-						decreaseQuestion={decreaseQuestion}
-						firstQuestion={firstQuestion}
-					/>
-				</>
-			) : window.location.href.includes("fragor") && lastPage ? (
-				<>
-					<ResultsComponent />
-				</>
-			) : window.location.href.includes("") ? (
-				<section className="persona-card-content">
-					<ImageComponent formImage={formImage} altImage={altImage} />
-					<PersonaContent activePersona={activePersona} />
-				</section>
-			) : null}
-		</section>
-	);
+  return (
+    <section className="card_content">
+      {window.location.href.includes("fragor") && firstPage ? (
+        <>
+          <OnboardingComponent startTest={startTest} />
+        </>
+      ) : window.location.href.includes("fragor") && !firstPage && !lastPage ? (
+        <>
+          <ImageComponent formImage={formImage} altImage={altImage} />
+          <ContentComponent
+            questions={questions}
+            formText={formText}
+            questionId={questionId}
+            increaseQuestion={increaseQuestion}
+            decreaseQuestion={decreaseQuestion}
+            firstQuestion={firstQuestion}
+          />
+        </>
+      ) : window.location.href.includes("fragor") && lastPage ? (
+        <>
+          <ResultsComponent />
+        </>
+      ) : window.location.href.includes("") ? (
+        <section className="persona-card-content">
+          <ImageComponent formImage={formImage} altImage={altImage} />
+          <PersonaContent activePersona={activePersona} />
+        </section>
+      ) : null}
+    </section>
+  );
 }
 
 export default FormComponent;
