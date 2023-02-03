d845867155cf4e0f9de57cc3c8ac304925f7b8cb
Monica Björk <m.bjork95@gmail.com>
2023-02-03 08:54:36 +0100

    typo

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -1,4 +1,4 @@
-import Footer from '../../components/footer/footer';
+import Footer from '../../components/footer/Footer';
 import './about.scss';
 import courseData from './tempInfoCourses.json';
 
