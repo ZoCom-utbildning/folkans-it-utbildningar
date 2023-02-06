feada807c05a6d37fee3d90bcf8784e9e1083a67
Ricky Johansson <Rickjoh91@hotmail.com>
2023-02-06 11:24:25 +0100

    onboarding adjustments

--- src/components/onboardingcomponent/onboardingComponent.tsx
+++ src/components/onboardingcomponent/onboardingComponent.tsx
@@ -8,13 +8,6 @@ const OnboardingComponent = ({ startTest }: Props) => {
 
     return (
         <div className="results_wrapper">
-            <section className="results_section">
-                <h2>Här är vår onboarding heading</h2>
-                <p>här är vår onboarding text</p>
-                <button className='start_button' onClick={() => startTest()}>
-                    Starta testet
-                </button>
-            </section>
             <section className="results_points">
                 <h2>Hur fungerar testet?</h2>
                 <p className='onboarding_text'>
@@ -23,6 +16,9 @@ const OnboardingComponent = ({ startTest }: Props) => {
                     När du svarat på frågorna kommer du att få ett resultat, där de tre utbildningar som du blivit bäst matchad mot dyker upp.
                     Detta är så klart bara en rekommendation, men det är samtidigt vår kvalificerade gissning för vilken utbildning vi tror är mest rätt för just dig.
                 </p>
+                <button className='start_button' onClick={() => startTest()}>
+                    Starta testet
+                </button>
             </section>
         </div>
     )
