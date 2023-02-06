e1a09ea91b08d581c90419f143e1c56de70bc78c
Ricky Johansson <Rickjoh91@hotmail.com>
2023-02-06 10:27:22 +0100

    onboarding text and margin fix

--- src/components/onboardingcomponent/onboardingComponent.tsx
+++ src/components/onboardingcomponent/onboardingComponent.tsx
@@ -17,12 +17,11 @@ const OnboardingComponent = ({ startTest }: Props) => {
             </section>
             <section className="results_points">
                 <h2>Hur fungerar testet?</h2>
-                <p className='onboarding_text'>
-                    Testet består av X frågor, som alla har tre svarsalternativ.
-                    Det finns inget rätt eller fel, utan svara med det svarsalternativ som känns mest rätt för hur just du ser på frågan.
-                    När du svarat på frågorna kommer du att få ett resultat, där de tre utbildningar som du blivit bäst matchad mot dyker upp.
-                    Detta är så klart bara en rekommendation, men det är samtidigt vår kvalificerade gissning för vilken utbildning vi tror är mest rätt för just dig.
-                </p>
+                <p className='onboarding_text'> All data från testet kommer att sparas anonymt. </p>
+                <p className='onboarding_text'> Testet består av X frågor, som alla har tre svarsalternativ. </p>
+                <p className='onboarding_text'> Det finns inget rätt eller fel, utan svara med det svarsalternativ som känns mest rätt för hur just du ser på frågan. </p>
+                <p className='onboarding_text'> När du svarat på frågorna kommer du att få ett resultat, där de tre utbildningar som du blivit bäst matchad mot dyker upp. </p>
+                <p className='onboarding_text'> Detta är så klart bara en rekommendation, men det är samtidigt vår kvalificerade gissning för vilken utbildning vi tror är mest rätt för just dig. </p>
             </section>
         </div>
     )
