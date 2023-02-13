1fdfa8a09831403758fa8d6f9763c1a509047799
Williamlof <83575469+Williamlof@users.noreply.github.com>
2023-02-13 11:12:02 +0100

    pushhh

--- src/components/personacontent/PersonaContent.tsx
+++ src/components/personacontent/PersonaContent.tsx
@@ -29,19 +29,19 @@ function PersonaContent({ activePersona, interviewData }: Props) {
       id: 2,
       personaTitle: "Ahmed, 32",
       personaIngress:
-        "Ahmed är tvåbarnsfadern med säljbakgrund som är mitt uppe i att sadla om till att bli programmerare. Han gick samhällsprogrammet på gymnasiet och har egentligen inte haft direkt erfarenhet av mjukvaruutveckling sedan tidigare, även om han faktiskt redan är yrkesverksam inom IT-branschen.",
+        "Ahmed är tvåbarnsfadern med säljbakgrund som är mitt uppe i att sadla om till att bli programmerare. Han gick samhällsprogrammet på gymnasiet och har inte haft erfarenhet av mjukvaruutveckling sedan tidigare, även om han redan är yrkesverksam inom IT-branschen.",
     },
     {
       id: 3,
       personaTitle: "Sonja, 22",
       personaIngress:
-        "Sonja är den nära på professionella gamern som programmerat sedan barnsben och nu tagit steget från att se utvecklingen som en hobby till att vara på god väg att bli en professionell utvecklare. Hon gick in med inställningen om att bara “formalisera” sina kunskaper till att inse att det finns mycket att lära i utbytet med andra, oavsett vilka de är.",
+        "Sonja är den nära på professionella gamern som programmerat sedan barnsben och nu tagit steget från att se utvecklingen som en hobby till att vara på god väg att bli en professionell utvecklare.",
     },
     {
       id: 4,
       personaTitle: "Emilija, 32",
       personaIngress:
-        "Emilija är karriärväxlaren som bytte katedern mot skolbänken och sadlade om från en karriär som SO-lärare på grundskolan till att nu vara helt inställd på programmering. Att hoppa på en programmeringsutbildning har givit henne en riktig nytändning i livet och nu kan hon räkna programmering som en av hennes största hobbies, även om hon stundtals får kämpa lite.",
+        "Emilija är karriärväxlaren som bytte katedern mot skolbänken och sadlade om från en karriär som SO-lärare på grundskolan till att nu vara helt inställd på programmering.",
     },
   ];
 
@@ -59,7 +59,7 @@ function PersonaContent({ activePersona, interviewData }: Props) {
                   <main className="persona-text-container">
                     <p className="persona-ingress">{persona.personaIngress}</p>
                     <button
-                      className="homeButton"
+                      className="personasButton"
                       onClick={(e) => navToPersonas(e)}
                     >
                       Läs hela intervjun här
