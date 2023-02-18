3a62703921104bc2220c7ee704a54f59dfef8a76
Monica Björk <m.bjork95@gmail.com>
2023-02-18 10:40:34 +0100

    moved the vector class to its own file

--- src/components/blob/Blob.tsx
+++ src/components/blob/Blob.tsx
@@ -3,57 +3,7 @@ import anime from 'animejs';
 import './blob.scss';
 import '../../scss/_variables.scss';
 import arrow from '../../assets/icons/arrowDown.svg';
-
-class Vector {
-    x: number;
-    y: number;
-    constructor({ x = 0, y = 0 }) {
-        this.x = x;
-        this.y = y;
-    }
-    add(...vectors: Vector[]) {
-        return vectors.reduce(
-            (acc, curr) => ( new Vector({
-                x: acc.x + curr.x,
-                y: acc.y + curr.y,
-            })),
-            new Vector({
-                x: this.x,
-                y: this.y,
-            })
-        );
-    }
-  
-    sub(p2: { x: number; y: number; }) {
-        return new Vector({
-            x: this.x - p2.x,
-            y: this.y - p2.y,
-        });
-    }
-  
-    mul(n: number) {
-        return new Vector ({
-            x: this.x*n,
-            y: this.y*n,
-        });
-    }
-  
-    dot(v2: this) {
-        return this.x*v2.x + this.y*v2.y;
-    }
-  
-    len() {
-        return Math.sqrt(this.dot(this));
-    }
-    
-    norm() {
-      let len = this.len();
-      return new Vector({
-        x: this.x/len,
-        y: this.y/len,
-      });
-    }
-}
+import {Vector} from './vector';
 
 function Blob(this: any) {
     // === Defining data ===
