6510e9490dbc7c2157c971649964747d6f477cb8
Monica Björk <m.bjork95@gmail.com>
2023-02-23 11:10:05 +0100

    Updated text and added button

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -5,6 +5,7 @@ import { db } from "../../services/firebase";
 import { collection, getDocs } from "firebase/firestore";
 import { EducationInfo } from '../../models/types';
 import Blob from '../../components/blob/Blob';
+import TestButton from '../../components/testbutton/testButton';
 
 function About() {
     const [courses, setCourses] = useState<EducationInfo[]>([]);
@@ -34,13 +35,23 @@ function About() {
     return (
         <div className="aboutView">
             <main>
-                <h1>YH-utbildningar</h1>
+                <h1>YH-utbildningar inom IT</h1>
                 <p className="ingress">Gå en yrkeshögskoleutbildning inom IT på <a href="https://www.folkuniversitetet.se/vara-skolor/yh-utbildningar/alla-yh-utbildningar/it-data/" target="_blank">Folkuniversitetet</a> som leder till jobb. Du kan till exempel utbilda dig till apputvecklare eller frontendutvecklare.</p>
                 <p>Alla YH-utbildningar är avgiftsfria, berättigar till studiemedel och sker i nära samverkan med arbetslivet. Utbildningarna innehåller en kvalificerad praktikperiod, LIA - lärande i arbete.</p>
-                <h2>Mål</h2>
-                <p>Målen med utbildningarna är att ge dig som studenter en yrkeskompetens som leder till arbete. Du får ett examensbevis när du genomgått utbildningen. När du är färdig med utbildningen har du mycket goda möjligheter till att få omedelbar anställning. Inte sällan på det företag där du genomfört din LIA-period.</p>
-                <h2>Innehåll</h2>
-                <p>Utbildningens innehåll består av både praktiska och teoretiska delar. Kursplaner och studieplaner tas fram tillsammans med näringslivets representanter. Under utbildningen har du utvecklingssamtal med dina lärare för att du ska få den feed-back du behöver för att utvecklas i din kommande yrkesroll.</p>
+
+                <h2>Om Folkuniversitetet</h2>
+                <p>Folkuniversitetet är en stor anordnare av yrkeshögskoleutbildningar. En YH-examen gör dig attraktiv inom ett yrkesområde där efterfrågan på professionell och kompetent personal är stor.</p>
+                <h3>Samarbete med näringslivet</h3>
+                <p>Våra yrkesutbildningar tas alltid fram i nära samarbete med näringslivet. Representanter från företag och organisationer sitter med i utbildningens ledningsgrupp och bidrar till att forma utbildningen. Det nära samarbetet innebär också att det finns tillgång till LIA-platser.  </p>
+                <h3>Mål</h3>
+                <p>Målen med utbildningarna är att ge dig som studerande en yrkeskompetens som leder till arbete. Du får ett examensbevis när du genomgått utbildningen. När du är färdig med utbildningen har du mycket goda möjligheter till att få omedelbar anställning. Inte sällan på det företag där du genomfört din LIA-period.</p>
+                <h3>Studerandeinflytande</h3>
+                <p>Alla våra yrkeshögskoleutbildningar har en ledningsgrupp. I dessa finns alltid representanter från Folkuniversitetet, från näringslivet och från de studerande. Särskilt viktiga är de synpunkter och åsikter om utbildningen som framförs av de studerande. Det är de som hjälper oss att förbättra och utveckla våra utbildningar. Förutom genom ledningsgruppen kan naturligtvis synpunkter kan framföras direkt till utbildningsledaren, lärare, studentrepresentanter eller genom de utvärderingar som görs flera gånger under utbildningen.</p>
+                <h3>Lärande i arbete</h3>
+                <p>Eller kort och gott LIA. Det är din praktikperiod. Den kan vara olika lång beroende på utbildning och sker ofta mot slutet av din utbildning. Då har du god nytta av vad du fått lära dig hittills, och kan snabbt sätta dig in i yrkesrollen. Du har naturligtvis studiemedel även under din LIA-period.</p>
+
+                <TestButton buttonText={'Gör Testet'} />
+                
             </main>
             <Blob xPos={1.2} yPos={1.8} radius={4.5} opacity={0.2} />
             <Footer />
