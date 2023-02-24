d487338d533440f8c0cb9c9e36b42677014cc2ca
KarlEmilArvid <emilarvidsson96@gmail.com>
2023-02-24 14:42:51 +0100

    remade /about

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -1,51 +1,47 @@
 import Footer from '../../components/footer/Footer';
-import './about.scss';
-import { useEffect, useState } from 'react';
-import { db } from "../../services/firebase";
-import { collection, getDocs } from "firebase/firestore";
-import { EducationInfo } from '../../models/types';
 import Blob from '../../components/blob/Blob';
+import './about.scss';
 
 function About() {
-    const [courses, setCourses] = useState<EducationInfo[]>([]);
-
-    useEffect(() => {
-        (async () => {
-            const querySnapshot = await getDocs(collection(db, "educations"));
-            const tempArr: any[] = [];
-            querySnapshot.forEach((doc) => {
-                tempArr.push(doc.data());
-            });
-
-            setCourses(tempArr);
-        })();
-        console.log(courses);
-    }, []);
-
-    const displayCourses: JSX.Element[] = courses.map((course: EducationInfo, index: number) => {
-        return (
-            <section className="course" key={index}>
-                <h3>{course.name}</h3>
-                <p>{course.location}</p>
-            </section>
-        );
-    });
 
     return (
         <div className="aboutView">
             <main>
-                <h1>YH-utbildningar</h1>
-                <p className="ingress">Gå en yrkeshögskoleutbildning inom IT på <a href="https://www.folkuniversitetet.se/vara-skolor/yh-utbildningar/alla-yh-utbildningar/it-data/" target="_blank">Folkuniversitetet</a> som leder till jobb. Du kan till exempel utbilda dig till apputvecklare eller frontendutvecklare.</p>
-                <p>Alla YH-utbildningar är avgiftsfria, berättigar till studiemedel och sker i nära samverkan med arbetslivet. Utbildningarna innehåller en kvalificerad praktikperiod, LIA - lärande i arbete.</p>
-                <h2>Mål</h2>
-                <p>Målen med utbildningarna är att ge dig som studenter en yrkeskompetens som leder till arbete. Du får ett examensbevis när du genomgått utbildningen. När du är färdig med utbildningen har du mycket goda möjligheter till att få omedelbar anställning. Inte sällan på det företag där du genomfört din LIA-period.</p>
-                <h2>Innehåll</h2>
-                <p>Utbildningens innehåll består av både praktiska och teoretiska delar. Kursplaner och studieplaner tas fram tillsammans med näringslivets representanter. Under utbildningen har du utvecklingssamtal med dina lärare för att du ska få den feed-back du behöver för att utvecklas i din kommande yrkesroll.</p>
+                <h1>Folkuniversitetet hjärta ZoCom</h1>
+                <p>På uppdrag från Folkuniversitetet och ZoCom har den här webbsidan designats och byggts av Emil Arvidsson, Monica Björk, Ricky Johansson och William Löfroth, som studerar till frontendutvecklare, med stöd från Vilhelm Stahl som studerar till IT-projektledare.</p>
+                <p>Denna sida är skapad under vår LIA period och för närmare kontakt så hittar ni oss här:</p>
+                <section className='about-section'>
+                    <section>
+                        <p>Emil Arvidsson</p>
+                        <a href="https://github.com/KarlEmilArvid">github</a>
+                        <a href="https://www.linkedin.com/in/karlemilarvid/">linkedin</a>
+                    </section>
+                    <section>
+                        <p>Monica Björk</p>
+                        <a href="https://github.com/MonBjo">github</a>
+                        <a href="https://www.linkedin.com/in/monbjo/">linkedin</a>
+                    </section>
+                    <section>
+                        <p>Ricky Johanson</p>
+                        <a href="https://github.com/RickyJohansson">github</a>
+                        <a href="https://www.linkedin.com/in/ricky-johansson-2264a9265/">linkedin</a>
+                    </section>
+                    <section>
+                        <p>William Löfroth</p>
+                        <a href="https://github.com/Williamlof">github</a>
+                        <a href="https://www.linkedin.com/in/william-l%C3%B6froth-aa2ba8263/">linkedin</a>
+                    </section>
+                    <section>
+                        <p>Vilhelm Stahl</p>
+                        <a href="https://github.com/VSMC">github</a>
+                        <a href="https://www.linkedin.com/in/vilhelm-stahl/">linkedin</a>
+                    </section>
+                </section>
             </main>
             <Blob xPos={1.2} yPos={1.8} radius={4.5} opacity={0.2} />
             <Footer />
         </div>
-    );
+    )
 }
 
-export default About;
+export default About
