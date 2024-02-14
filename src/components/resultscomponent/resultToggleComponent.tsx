5b9bb1e294950e672677921770e21b9c04d28edf
Fabiola Poblete <fabiola.broccolo@gmail.com>
2024-02-14 09:49:55 +0100

    Changed direction, position and styling for arrows in result section

--- src/components/resultscomponent/resultToggleComponent.tsx
+++ src/components/resultscomponent/resultToggleComponent.tsx
@@ -2,51 +2,66 @@ import { useState, useEffect } from 'react';
 import { CourseScore, Links } from '../../models/types';
 
 type Props = {
-    courseScore: CourseScore;
-    index: number;
-}
+  courseScore: CourseScore;
+  index: number;
+};
 
 const ResultToggleComponent = ({ courseScore, index }: Props) => {
-    const [toggleInfo, setToggleInfo] = useState<boolean>(false);
+  const [toggleInfo, setToggleInfo] = useState<boolean>(false);
 
-    useEffect(() => {
-        if (index === 0) {
-            setToggleInfo(true);
-        }
-    }, [])
-
-    const openNewTab = (link: string) => {
-        const newWindow = window.open(link, '_blank', 'noopener,noreferrer');
-        if (newWindow) { newWindow.opener = null }
+  useEffect(() => {
+    if (index === 0) {
+      setToggleInfo(true);
     }
+  }, []);
 
-    return (
-        <section className="results_list">
-            <section key={index} className="results_item">
-                <h2 className="rank">{`${index + 1}`}</h2>
-                <span onClick={() => setToggleInfo(!toggleInfo)}>{courseScore.course}
-                    <span className="study_at">{courseScore.studyAt}
-                        {toggleInfo ?
-                            <span className='toggled' ></span>
-                            :
-                            <span className='notToggled' ></span>
-                        }
-                    </span>
-                </span>
-                <button className='read-more-button' onClick={() => openNewTab(`/utbildningar/${courseScore.intLink}`)}> Läs mer </button>
-            </section>
-            {
-                toggleInfo ?
-                    <section className="toggleText">
-                        <p className="toggleTextTitle"> {courseScore.eduTextTitle} </p>
-                        <p> {courseScore.eduText} </p>
-                        <a className='apply-link' onClick={() => openNewTab(courseScore.links.link)}> Ansök här </a>
-                    </section>
-                    :
-                    ''
-            }
+  const openNewTab = (link: string) => {
+    const newWindow = window.open(link, '_blank', 'noopener,noreferrer');
+    if (newWindow) {
+      newWindow.opener = null;
+    }
+  };
+
+  return (
+    <section className='results_list'>
+      <section key={index} className='results_item'>
+        <h2 className='rank'>{`${index + 1}`}</h2>
+        <span onClick={() => setToggleInfo(!toggleInfo)}>
+          {courseScore.course}
+          <span className='study_at'>
+            {courseScore.studyAt}
+            {toggleInfo ? (
+              <span className='toggled'></span>
+            ) : (
+              <span className='notToggled'></span>
+            )}
+          </span>
+        </span>
+        <button
+          className='read-more-button'
+          onClick={() => openNewTab(`/utbildningar/${courseScore.intLink}`)}
+        >
+          {' '}
+          Läs mer{' '}
+        </button>
+      </section>
+      {toggleInfo ? (
+        <section className='toggleText'>
+          <p className='toggleTextTitle'> {courseScore.eduTextTitle} </p>
+          <p> {courseScore.eduText} </p>
+          <a
+            className='apply-link'
+            onClick={() => openNewTab(courseScore.links.link)}
+          >
+            {' '}
+            Ansök här{' '}
+          </a>
         </section>
-    )
-}
+      ) : (
+        ''
+      )}
+    </section>
+  );
+};
 
 export default ResultToggleComponent;
