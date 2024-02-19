8cdf952c72be0f8476011a0aea5aaca8d1f18bf7
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2024-02-19 11:06:09 +0100

    fix: animation

--- src/components/transitionWrapper/transitionWrapper.tsx
+++ src/components/transitionWrapper/transitionWrapper.tsx
@@ -8,11 +8,11 @@ interface TransitionWrapperProps {
 
 export const TransitionWrapper: React.FC<TransitionWrapperProps> = ({ children }) => {
     const location = useLocation();
-    const formP = document.querySelectorAll('.results_points');
   useEffect(() => {
     let timeLine = anime.timeline({
         easing: 'easeOutExpo',autoplay:true, loop:false,
     });
+    
     
     //transition wrapper animation 
     timeLine.add( {
@@ -21,26 +21,15 @@ export const TransitionWrapper: React.FC<TransitionWrapperProps> = ({ children }
         translateX: [500, 0],
         duration: 500,
       },100);
-    
-      //Html elements' animation with staggering effect   
-    timeLine.add({
-        targets: 'p:not(.personas-card-text,.ingress),.coursesWrapper,h1,h2:not(.question-title,.personas-card-title)', 
-        opacity: [0, 1],
-        translateX: [200, 0],
-        duration: 1500,
-        delay: anime.stagger(200),
-      },500);
 
-      //Buttons and test's container animation  
+      //Home button animation 
       timeLine.add({
-        targets: '.card_content,button',
+        targets: '.homeButton',
         translateY: [50, 0],
         opacity: [0, 1],
         duration: 1500,
         delay: anime.stagger(200),
-      },600);
-
-
+      },500);
     
   }, [location.pathname]); // refresh the animation everytime the pathname is changed 
 
